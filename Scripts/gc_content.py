df9efc86aa48c2dc6c42818e370b55a55e0d33bb
Dominique <dominique.massau@gmail.com>
2020-09-22 12:26:49 +0200

    Create function parse_fasta

--- Scripts/gc_content.py
+++ Scripts/gc_content.py
@@ -5,10 +5,31 @@ Author: Dominique Massau
 """
 
 # imports
+from sys import argv
 
 
 # functions
+def parse_fasta(file_name: str) -> dict:
+    """
+    Parse a fasta file into a dictionary
+
+    :param file_name: str, name of the fasta file
+    :return: dict, contains fasta header: sequence
+    """
+
+    with open(file_name, 'r') as lines:
+        fasta_dict = {}
+        for line in lines:
+            if line.startswith('>'):
+                current_header = line.strip('>').strip()
+                fasta_dict[current_header] = []
+            else:
+                fasta_dict[current_header].append(line.strip())
+
+    for header, seq in fasta_dict.items():
+        fasta_dict[header] = ''.join(seq)
+    return fasta_dict
 
 
 if __name__ == "__main__":
-    pass
+    print(parse_fasta(argv[1]))
