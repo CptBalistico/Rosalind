b5f02ddd1a043fafc1c4341805858e6890e558a5
Dominique <dominique.massau@gmail.com>
2020-09-22 12:05:25 +0200

    Create gc_content script

---
+++ Scripts/gc_content.py
@@ -0,0 +1,14 @@
+"""
+Compute the GC content and report the sequence with the highest content
+
+Author: Dominique Massau
+"""
+
+# imports
+
+
+# functions
+
+
+if __name__ == "__main__":
+    pass
