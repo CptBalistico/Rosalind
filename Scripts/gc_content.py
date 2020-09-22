850cd713766c47a4396e95885186360e9de11c75
Dominique <dominique.massau@gmail.com>
2020-09-22 13:01:00 +0200

    Create function highest_gc_perc

--- Scripts/gc_content.py
+++ Scripts/gc_content.py
@@ -36,16 +36,28 @@ def calculate_gc_content(fasta_dict: dict) -> list:
     Calculate the GC percentage for every fasta sequence
 
     :param fasta_dict:
-    :return: list, contains tuple with fasta header: GC perc
+    :return: list, contains tuple with fasta header, GC perc
     """
 
     gc_perc_list = []
     for header, seq in fasta_dict.items():
         gc_perc_list.append((header, (seq.count('c') + seq.count('g'))
-                             / len(seq)))
+                             / len(seq) * 100))
     return gc_perc_list
+
+
+def highest_gc_perc(gc_perc_list: list):
+    """
+    Reports the header of the sequence with the largest GC percentage
+
+    :param gc_perc_list: list, contains tuple with fasta header, GC perc
+    """
+
+    highest_gc_fasta = sorted(gc_perc_list, key=lambda tup: tup[1])[-1]
+    print("{}\n{:.6f}".format(highest_gc_fasta[0], highest_gc_fasta[1]))
 
 
 if __name__ == "__main__":
     fasta_seqs = parse_fasta(argv[1])
     gc_perc_list = calculate_gc_content(fasta_seqs)
+    highest_gc_perc(gc_perc_list)
