68c5a8b37a6c86dc833053ff85b3ee59d4d25afb
Dominique <dominique.massau@gmail.com>
2020-09-22 12:41:06 +0200

    Create function calculate_gc_content

--- Scripts/gc_content.py
+++ Scripts/gc_content.py
@@ -14,7 +14,7 @@ def parse_fasta(file_name: str) -> dict:
     Parse a fasta file into a dictionary
 
     :param file_name: str, name of the fasta file
-    :return: dict, contains fasta header: sequence
+    :return: dict, contains fasta header: DNA sequence
     """
 
     with open(file_name, 'r') as lines:
@@ -24,12 +24,28 @@ def parse_fasta(file_name: str) -> dict:
                 current_header = line.strip('>').strip()
                 fasta_dict[current_header] = []
             else:
-                fasta_dict[current_header].append(line.strip())
+                fasta_dict[current_header].append(line.strip().lower())
 
     for header, seq in fasta_dict.items():
         fasta_dict[header] = ''.join(seq)
     return fasta_dict
 
 
+def calculate_gc_content(fasta_dict: dict) -> list:
+    """
+    Calculate the GC percentage for every fasta sequence
+
+    :param fasta_dict:
+    :return: list, contains tuple with fasta header: GC perc
+    """
+
+    gc_perc_list = []
+    for header, seq in fasta_dict.items():
+        gc_perc_list.append((header, (seq.count('c') + seq.count('g'))
+                             / len(seq)))
+    return gc_perc_list
+
+
 if __name__ == "__main__":
-    print(parse_fasta(argv[1]))
+    fasta_seqs = parse_fasta(argv[1])
+    gc_perc_list = calculate_gc_content(fasta_seqs)
