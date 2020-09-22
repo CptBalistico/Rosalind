0e9a5fa25f57e2bc6c98980a5b2970ede2cb977f
Dominique <dominique.massau@gmail.com>
2020-09-22 11:39:48 +0200

    Create script reverse_complement

---
+++ Scripts/reverse_complement.py
@@ -0,0 +1,15 @@
+"""
+Script to produce the reverse complement of a DNA sequence
+
+Author: Dominique Massau
+Date: 22/09/2020
+"""
+
+# imports
+
+
+# functions
+
+
+if __name__ == "__main__":
+    pass
