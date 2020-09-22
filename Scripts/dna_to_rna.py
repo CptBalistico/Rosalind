e82437edcebe15cb4371b36c35774ce50873e386
Dominique Massau <dominique.massau@gmail.com>
2020-09-22 10:57:47 +0200

    Create function convert_dna_to_rna

--- Scripts/dna_to_rna.py
+++ Scripts/dna_to_rna.py
@@ -5,6 +5,25 @@ Author: Dominique Massau
 Date: 22/09/2020
 """
 
+# imports
+from nucleotide_count import parse_dna_string
+from sys import argv
+
+
+def convert_dna_to_rna(dna_str: str) -> str:
+    """
+    Convert a DNA sequence into an RNA sequence
+
+    :param dna_str: str, sequence of a DNA sequence
+    :return: str, sequence of a corresponding RNA segment
+    """
+
+    nucleotide_list = ['u' if nucleotide == 't' else nucleotide
+                       for nucleotide in dna_str]
+    return ''.join(nucleotide_list).upper()
+
 
 if __name__ == "__main__":
-    pass
+    dna_string = parse_dna_string(argv[1])
+    rna_string = convert_dna_to_rna(dna_string)
+    print(rna_string)
