6a12bb623b028682aea2fd074da36e58cfb9cbcd
Dominique <dominique.massau@gmail.com>
2020-09-27 16:43:47 +0200

    Create functions parse_dna_seq and identify_mutations

---
+++ Scripts/point_mutation_identification.py
@@ -0,0 +1,32 @@
+"""
+Identify point mutations and report the hamming distance
+
+Author: Dominique Massau
+Date: 27/09/2020
+"""
+
+# imports
+from nucleotide_count import is_valid_sequence
+from sys import argv
+
+
+# functions
+def parse_dna_seq(text_file: str) -> list:
+    """
+    Parse one or more DNA sequences in a list
+
+    :param text_file: str, name of the file with DNA sequences
+    :return: list, contains DNA sequences
+    """
+
+
+def identify_mutations(sequences: list) -> int:
+    """
+    Identify the count of point mutations between two sequences
+    :param sequences: list, contains two sequences (strings)
+    :return: int, the hamming distance between the two sequences
+    """
+
+
+if __name__ == "__main__":
+    pass
