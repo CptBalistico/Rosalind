7dcad5b9df5092e97695ac56205f5c54129525b7
Dominique <dominique.massau@gmail.com>
2020-09-27 17:33:57 +0200

    Create function check_pairs

--- Scripts/error_correction.py
+++ Scripts/error_correction.py
@@ -12,7 +12,31 @@ from sys import argv
 
 
 # functions
+def check_pairs(fastas: dict):
+    """
+    Check whether at least two pairs are present
+
+    :param fastas: dict, contains header: sequence
+    :return:
+    """
+
+    pairs = {}
+
+    for seq in fastas.values():
+        reverse_comp = get_reverse_complement(seq).lower()
+        if seq in pairs:
+            pairs[seq].append(seq)
+        elif reverse_comp in pairs:
+            pairs[reverse_comp].append(seq)
+        else:
+            pairs[seq] = []
+
+    # Separately store reads that occur only once
+    error_reads = [seq_1 for seq_1, seq_2 in pairs.items() if seq_2 == []]
+
+    return error_reads, fastas.values()
 
 
 if __name__ == "__main__":
     fasta_dict = parse_fasta(argv[1])
+    check_pairs(fasta_dict)
