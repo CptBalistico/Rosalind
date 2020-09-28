9bf7708de221667bb80067d9a793dac4167fa4db
Dominique <dominique.massau@gmail.com>
2020-09-28 10:26:36 +0200

    Minor changes

--- Scripts/point_mutation_identification.py
+++ Scripts/point_mutation_identification.py
@@ -18,6 +18,11 @@ def parse_dna_seq(text_file: str) -> list:
     :param text_file: str, name of the file with DNA sequences
     :return: list, contains DNA sequences
     """
+    sequences = []
+    with open(text_file, 'r') as lines:
+        for line in lines:
+            sequences.append(line.strip())
+    return sequences
 
 
 def identify_mutations(sequences: list) -> int:
@@ -27,6 +32,16 @@ def identify_mutations(sequences: list) -> int:
     :return: int, the hamming distance between the two sequences
     """
 
+    hamming_distance = 0
+    for char_1, char_2 in zip(sequences[0], sequences[1]):
+        if char_1 != char_2:
+            hamming_distance += 1
+    return hamming_distance
+
 
 if __name__ == "__main__":
-    pass
+    dna_sequences = parse_dna_seq(argv[1])
+    for sequence in dna_sequences:
+        if not is_valid_sequence(sequence):
+            raise ValueError("Invalid characters in sequence")
+    print(identify_mutations(dna_sequences))
