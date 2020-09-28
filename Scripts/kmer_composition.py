b5a96dc9651ec9c067e908ef8bfb15fee03528dd
Dominique <dominique.massau@gmail.com>
2020-09-28 10:42:26 +0200

    Create create_kmers, kmer_occurence, report_occurrence

--- Scripts/kmer_composition.py
+++ Scripts/kmer_composition.py
@@ -11,6 +11,32 @@ from sys import argv
 
 
 # functions
+def create_kmers(sequence: str, kmer_size: int) -> list:
+    """
+    Create a list with k-mers of a specified size from a DNA sequence
+
+    :param sequence: str, a DNA sequence
+    :param kmer_size: int, the k-mer size
+    :return: list, contains k-mers
+    """
+
+
+def kmer_occurence(sequence: str, kmers: list) -> dict:
+    """
+    Count the occurence of each kmer in a string
+
+    :param sequence: str, a DNA sequence
+    :param kmers: list, a collection of k-mers
+    :return: dict, the occurrence of each k-mer in the string
+    """
+
+
+def report_occurence(kmer_occurence: dict):
+    """
+    Report the occurrence of each k-mer in lexicographical order
+
+    :param kmer_occurence:
+    """
 
 
 if __name__ == "__main__":
