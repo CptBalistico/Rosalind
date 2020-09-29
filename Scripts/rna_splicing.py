7bc0d83a0aed4257017affdcd898fdd73099ebe1
Dominique <dominique.massau@gmail.com>
2020-09-29 19:11:38 +0200

    Create script rna_splicing

---
+++ Scripts/rna_splicing.py
@@ -0,0 +1,17 @@
+"""
+Script to excise intronic sequences from DNA and translate it to a protein
+
+Author: Dominique Massau
+Date: 29/09/2020
+"""
+
+# imports
+from sys import argv
+
+
+# functions
+
+
+
+if __name__ == "__main__":
+    fasta_list = parse_fasta(argv[1])
