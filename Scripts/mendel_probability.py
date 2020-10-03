119341422331bcefd5e02be2db9215411868eb5e
Dominique <dominique.massau@gmail.com>
2020-10-03 16:50:31 +0200

    Implement functionality to function parse_numbers

--- Scripts/mendel_probability.py
+++ Scripts/mendel_probability.py
@@ -11,23 +11,29 @@ from sys import argv
 
 
 # functions
-def parse_numbers(text_file: str) -> tuple:
+def parse_numbers(text_file: str) -> list:
     """
     Parse the number of individuals with specific alleles
 
-    Three numbers are given in a file on the same line (k, m and n). The
+    Three numbers are given in a file on the same line (k m n). The
     number k represents homozygous dominant organisms, m represents
     heterozygous organisms and n represents homozygous recessive organisms
     The numbers are parsed from the first line of the file and returned
-    in a tuple (in order: k, m. n)
-
+    in a tuple (in order: k, m, n)
 
     :param text_file: str, name of the text file
-    :return: tuple, number of organisms with a specific allele
+    :return: list, number of organisms with a specific allele
     """
 
+    with open(text_file, 'r') as lines:
+        for line in lines:
+            if line.strip() == '':
+                continue
+            else:
+                return line.split(' ')
 
-def dominant_probability(individuals: tuple) -> float:
+
+def dominant_probability(individuals: list) -> float:
     """
     Calculate the probability of an individual having a dominant allele
 
@@ -36,10 +42,11 @@ def dominant_probability(individuals: tuple) -> float:
     organisms (n) from the input, the probability is calculated that an
     organism has at least one dominant allele
 
-    :param individuals: tuple, number of organisms with a specific allele
+    :param individuals: list, number of organisms with a specific allele
     :return: float, probability of an organism having a dominant allele
     """
 
 
 if __name__ == "__main__":
-    parse_numbers(argv[1])
+    numbers = parse_numbers(argv[1])
+    print(numbers)
